094b69f636112b853551a7178227a4860694e1fa
aguspranyoto <agusprnyt@gmail.com>
2023-05-22 11:49:50 +0700

    second commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Agus Pranyoto"; // TODO: replace this
+
+const studentId = "FE4299766"; // TODO: replace this
+
+const NetlifyDeployUrl = "TODO: replace me"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
